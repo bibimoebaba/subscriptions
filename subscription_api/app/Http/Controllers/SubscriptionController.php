e2ef5287217b0c072b21d6a4f014e9d0c251ef3b
Dennis Sloove <dennissloove1995@hotmail.nl>
2018-02-28 10:27:28 +0100

    meer comments toegevoegd

--- subscription_api/app/Http/Controllers/SubscriptionController.php
+++ subscription_api/app/Http/Controllers/SubscriptionController.php
@@ -6,19 +6,24 @@ use \App\Subscription;
 
 class SubscriptionController extends Controller
 {
+
+    //Return all records
     public function index(){
         return(Subscription::all());
     }
 
+    //Return single record
     public function show($id){
         return(Subscription::findOrFail($id));
     }
 
+    //Store record in database
     public function store(Request $request){
         $this->validate_request($request);
         Subscription::create($request->all());
     }
 
+    //Update a single record
     public function update(Request $request, $id){
         $this->validate_request($request);
         $subscription = Subscription::findOrFail($id);
@@ -26,16 +31,17 @@ class SubscriptionController extends Controller
         return response()->json(['subscription', $subscription]);
     }
 
+    //Delete a single record
     public function delete($id){
         $subscription = Subscription::findOrFail($id);
         $subscription->delete();
         return(true);
     }
 
+    //Function to validate te request
     private function validate_request($request){
         $this->validate($request, [
-            'name' => 'required',
-            'description' => 'required',
+            'name' => 'required|min:1',
             'origin_name' => 'required'
         ]);
     }
